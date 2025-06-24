ca6a1cc723f9cae3f47419ed3cb17b7bb5092d0f
ghackev <47868874+ghackev@users.noreply.github.com>
2025-06-24 19:28:34 -0300

    Use Clerk middleware and apply guard at controller

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(ClerkExpressWithAuth());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
