7bc436adb26d0bc6dd4440276a0a8275fd17c900
ghackev <47868874+ghackev@users.noreply.github.com>
2025-06-26 09:15:32 -0300

    Add NestJS backend with Clerk auth and transactions

---
+++ backend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ClerkAuthGuard } from './auth/clerk-auth.guard';
+import { TransactionsModule } from './transactions/transactions.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    TransactionsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ClerkAuthGuard,
+    },
+  ],
+})
+export class AppModule {}
