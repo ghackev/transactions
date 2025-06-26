fe21a7146696a6cc251981b08a338a29f8c5249f
Germán Hackembruch <ghackev@gmail.com>
2025-06-26 09:05:58 -0300

    Remove

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransactionsModule } from './transactions/transactions.module';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [TransactionsModule, AuthModule],
-})
-export class AppModule {}
